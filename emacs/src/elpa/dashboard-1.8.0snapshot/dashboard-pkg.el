8dd04099fe5fa7d74de1c7bf25b276997a037cf6
Satya Prakash Dahal <dahal.satya321@gmail.com>
2022-12-06 12:24:54 +0545

    Removed unnessary fiels

--- emacs/src/elpa/dashboard-1.8.0snapshot/dashboard-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from dashboard.el  -*- no-byte-compile: t -*-
-(define-package "dashboard" "1.8.0snapshot" "A startup screen extracted from Spacemacs" '((emacs "26.1")) :authors '(("Rakan Al-Hneiti" . "rakan.alhneiti@gmail.com")) :maintainer '(("Jesús Martínez" . "jesusmartinez93@gmail.com") ("Jen-Chieh" . "jcs090218@gmail.com")) :keywords '("startup" "screen" "tools" "dashboard") :url "https://github.com/emacs-dashboard/emacs-dashboard")
