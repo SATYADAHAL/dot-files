8a0aed0e8b51a5765f0272198abb04fb3b910e6b
Satya Prakash Dahal <dahal.satya321@gmail.com>
2022-12-06 12:32:58 +0545

    Changed Dired mode to minimal interface\nAdded icons to the Dired mode

---
+++ emacs/src/dired.el
@@ -0,0 +1,15 @@
+;;; EmacsRule --- For Config
+;;; Commentary:
+;;; Code:
+
+;;; Load Melpa(Package Manager)
+
+(use-package all-the-icons-dired
+  :ensure t)
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
+(require 'dired)
+(add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode 1)))
+
+(provide 'dired)
+;;; dired.el ends here
